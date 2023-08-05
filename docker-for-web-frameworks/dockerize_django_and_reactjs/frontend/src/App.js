0f5168ff9117876af990391d302fd6786243aadc
dhirajkafle553 <dhirajkafle553@gmail.com>
2023-08-05 11:40:14 +0545

    Dockerize web frameworks

---
+++ docker-for-web-frameworks/dockerize_django_and_reactjs/frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wallaha Habibi ! Sumulya Gold Habibi khabibi mabibi  HELLO DHIRAAA !
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
